b058dc9d2d702186d95de37f0756aaba31b003a3
Vadim Pyadukhov <vpyadukhov@outlook.com>
2018-10-03 00:06:39 +0300

    add region webapi controller

---
+++ PaymentProviders-Web-API/WebApiControllers/PaymentRegionApiController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PaymentProviders_Web_API.DbContexts;
+using PaymentProviders_Web_API.Models.WebApi.PaymentsProviders;
+
+namespace PaymentProviders_Web_API.WebApiControllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class PaymentRegionApiController : ControllerBase
+    {
+        private PaymentProvidersContext providersContext;
+
+        public PaymentRegionApiController()
+        {
+            providersContext = new PaymentProvidersContext();
+        }
+
+        // GET: v1/PaymentRegionApi
+        [HttpGet]
+        public ActionResult<IEnumerable<PaymentRegion>> Get(string q)
+        {
+            List<PaymentRegion> regions;
+
+            if (string.IsNullOrEmpty(q))
+            {
+                regions = providersContext.PaymentRegions.ToList();
+            }
+            else
+            {
+                regions = providersContext.PaymentRegions.Where(x => x.Name.ToLower().Contains(q.ToLower())).ToList();
+            }
+
+            if (regions == null || regions.Count == 0)
+                return NotFound();
+            else
+                return regions;
+        }
+    }
+}
